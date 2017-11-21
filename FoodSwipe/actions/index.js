90316268c8501cfff7dbcdf1be0c8b4126332bcc
Donald Stolz <donaldtstolz@gmail.com>
2017-11-21 15:11:38 -0600

    Updating actions

--- FoodSwipe/actions/index.js
+++ FoodSwipe/actions/index.js
@@ -2,12 +2,11 @@
 Action List:
 
 SEARCH_RESTRAUNTS
-GET_RESTRAUNT_INFO
-
 NOT_A_MATCH
+
 ADD_FAVORITE
 REMOVE_FAVORITE
-GET_FAVORITES
+
 */
 
 // action types
@@ -16,7 +15,6 @@ export const NOT_A_MATCH = 'NOT_A_MATCH'
 
 export const ADD_FAVORITE = 'ADD_FAVORITE'
 export const REMOVE_FAVORITE = 'REMOVE_FAVORITE'
-// export const GET_FAVORITES = 'GET_FAVORITES'?
 
 // Filter constants
 
@@ -29,13 +27,14 @@ export const RestrauntFilters = {
 
 // action creators
 	/* Restraunts */
-export function filterRestraunts(/*filter*/){
+export function fecthRestraunts(/*filter*/){
 	// TODO
 	return { type: FETCH_RESTRAUNTS, filter }
 }
 
-export function getRestraunt(/*id*/){
+export function noMatch(/*id*/) {
 	// TODO
+  return { type: NOT_A_MATCH, text }
 }
 
 	/* List */
@@ -44,17 +43,7 @@ export function addFavorite(/*id*/) {
   return { type: ADD_FAVORITE, text }
 }
 
-export function noMatch(/*id*/) {
-	// TODO
-  return { type: NOT_A_MATCH, text }
-}
-
 export function removeFavorite(/*id*/) {
 	// TODO
   return { type: REMOVE_FAVORITE, text }
-}
-
-export function getFavorites(/*?*/) {
-	// TODO
-  return { type: GET_FAVORITES, text }
 }
