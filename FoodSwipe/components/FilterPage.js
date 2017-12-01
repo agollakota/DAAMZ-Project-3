66a6e59391f28ab19c698912ea9b44781326b0f4
Donald Stolz <donaldtstolz@gmail.com>
2017-11-30 18:19:46 -0600

    Work on filter page

--- FoodSwipe/components/FilterPage.js
+++ FoodSwipe/components/FilterPage.js
@@ -3,7 +3,9 @@ import {
     StyleSheet,
     Text,
     View,
-		Picker
+		Picker,
+		Slider,
+		TouchableOpacity
 } from 'react-native'
 import CheckBox from 'react-native-check-box'
 
@@ -21,16 +23,14 @@ export default class example extends Component {
 												{name : "Gluten Free", checked:false}],
 				selectedDiet : [],
 				distance : 1600, // Meters; So default close to a mile
-				price : { min : 0, max : 4}
+				price : 5
 			}
     }
 
 		renderPicker(){
 			let items = this.state.foodOptions.map((type, index) => {
-				return  <Picker.Item label={type} value={index} />;
+				return  <Picker.Item label={type} value={index} key={index} />;
 			})
-
-			const options = this.state.foodOptions;
 
 			return(
 				<Picker
@@ -48,6 +48,7 @@ export default class example extends Component {
 						onClick={()=>this.onClickCheckBox(filter)}
 						isChecked={filter.checked}
 						leftText={filter.name}
+						key={index}
 				/>
 			})
 			return boxes;
@@ -69,35 +70,77 @@ export default class example extends Component {
     }
 
 		renderDistanceSlider(){
-
+			return (
+				<Slider
+	         step={100}
+	         minimumValue={500}
+	         maximumValue={5000}
+	         value={this.state.distance}
+	         onValueChange={val => this.setState({ distance: val })}
+	        />);
 		}
 
 		renderPriceOptions(){
-
+			return (
+				<Slider
+					 step={1}
+					 minimumValue={1}
+					 maximumValue={5}
+					 value={this.state.price}
+					 onValueChange={val => this.setState({ price: (val) })}
+					/>);
 		}
 
 		renderView() {
 
 				views = (
-					<View>
-						<Text>{"Type of Food:"}</Text>
+					<View style={styles.filters}>
+						<Text style={styles.headers}>{"Type of Food:"}</Text>
 							{this.renderPicker()}
-						<Text>{"Diet Restrictions:"}</Text>
+						<Text style={styles.headers}>{"Diet Restrictions:"}</Text>
 							{this.renderCheckBox()}
-						<Text>{"Distance:"}</Text>
-							{}
-						<Text>{"Price:"}</Text>
-							{}
+						<Text style={styles.headers}>{"Distance: " + this.state.distance + "M"}</Text>
+							{this.renderDistanceSlider()}
+						<Text style={styles.headers}>{"Price: "+ (this.state.price)}</Text>
+							{this.renderPriceOptions()}
+
 					</View>
 				)
         return views;
 
     }
 
+		onPressButton = () => {
+			const food = this.state.foodOptions[this.state.selectedFoodIndex];
+			// var diet = ' '
+			// this.state.dietOptions.map((filter, index) => {
+			// 	if (filter.checked) {
+			// 		diet.concat(filter.name)
+			// 		console.log(filter.name);
+			// 	}
+			// })
+			// console.log("Diet: " + diet);
+			const price = this.state.price;
+			const distance = this.state.distance;
+
+			var request = {
+				query : food,
+				minPriceLevel : 0,
+				maxPriceLevel : price,
+				type : 'restaurant',
+				// location : userLocation,
+				radius : distance
+			}
+			console.log(request);
+		}
+
     render() {
         return (
             <View style={styles.container}>
               {this.renderView()}
+							<TouchableOpacity style={styles.button} onPress={this.onPressButton}>
+					      <Text style={styles.buttonText}>Search</Text>
+					    </TouchableOpacity>
             </View>
         )
     }
@@ -109,5 +152,23 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#f3f2f2',
         marginTop:30
-    }
+    },
+		filters: {
+			flex: 4,
+			padding: 25
+		},
+		headers: {
+			fontSize: 20,
+			fontWeight: 'bold'
+		},
+		button: {
+			backgroundColor: '#3fffff',
+			flex: 1,
+			margin: 50,
+			padding: 20
+		},
+		buttonText:{
+			fontSize: 50,
+			textAlign: 'center'
+		}
 })
