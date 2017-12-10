801d00fdbdea6c39676541c116da551f9d6294a3
msukan <31421218+msukan@users.noreply.github.com>
2017-12-10 12:43:30 -0600

    Update App.js
    Integrate App.js with map of default location.

--- App.js
+++ App.js
@@ -1,16 +1,91 @@
+
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { Header } from './components/common';
-import { Input } from './components/common';
-class App extends Component {
-  render() {
-    return(
-      <View>
-        <Header headerText="Authentication" />
-        <Text> FoodSwap </Text>
-        </View>
-      );
+import { View } from 'react-native';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import { NativeRouter, Route } from 'react-router-native'
+import thunk from 'redux-thunk'
+import { AppRegistry, StyleSheet, Text, View, Dimensions} from "react-native";
+//import {StyleSheet, Text, View} from 'react-native';
+import MapView from 'react-native-maps';
+
+//import MapView, {PROVIDER_GOOGLE} from "react-native-maps";
+import reducer from './reducers'
+
+import Header from './components/Header'
+import FilterContainer from './containers/FilterContainer'
+import CardsContainer from './containers/CardsContainer'
+// import FavoritesList from './containers/FavoritesList'
+
+const store = createStore(reducer, applyMiddleware(thunk));
+var lat =  42.00;
+var longitude = -87.66;
+
+export default class App extends React.Component{
+ /* render() {
+    return (
+			<Provider store={store}>
+				<NativeRouter>
+					<View style={{flex: 1}}>
+						<Header headerText={"Food Swipe"}/>
+						<Route exact path="/" component={FilterContainer}/>
+						<Route path="/cards" component={CardsContainer}/>
+					</View>
+				</NativeRouter>
+			</Provider>
+    );
   }
 }
+*/
+render()   {
+      return (
+      <View style={styles.container}>
 
-export default App;
+         <MapView style={styles.map}
+              region={{
+                  latitude: lat,
+                  longitude: longitude,
+                  latitudeDelta: 0.1,
+                  logitudeDelta: 0.1
+
+                 }}
+
+         >
+
+            <MapView.Marker
+                 coordinate={{
+                   latitude: lat,
+                   longitude: long
+                   }}
+                 title={'You are here'}
+                 />
+          </MapView>
+        </View>
+    );
+  }
+
+}
+
+
+cons styles = StyleSheet.create({
+
+     container:{
+         position: 'absolute',
+         top: 0,
+         left: 0,
+         bottom: 0,
+         right: 0,
+         justifyContent: 'flex-end',
+         alignItems: 'center'
+      },
+
+     map: {
+         position: 'absolute',
+         top: 0,
+         left: 0,
+         bottom: 0,
+         right: 0
+     }
+});
+
+// TODO: <Route path="/favorites" component={FavoritesList}/>
