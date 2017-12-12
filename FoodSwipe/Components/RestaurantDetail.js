f11990c752c1e7d6d2d9d8fff064e5308229f2eb
Austin <aedington@luc.edu>
2017-12-11 19:33:34 -0600

    importing restaurant details ((test))

--- FoodSwipe/Components/RestaurantDetail.js
+++ FoodSwipe/Components/RestaurantDetail.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import {
+	StyleSheet,
+	Text,
+	View,
+	Image
+} from 'react-native';
 import Card from './common/Card';
 import CardSection from './common/CardSection';
-
+import {restaurants} from './actions/RestrauntActions';
 
 const RestaurantDetail = ({ restaurant }) => {
   const { name, rating } = restaurant;
