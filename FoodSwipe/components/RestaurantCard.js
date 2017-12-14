78bdb262dd8be04c6bb0a2da71dc013ff168b9d6
donald-stolz <donaldtstolz@gmail.com>
2017-12-13 21:22:23 -0600

    Added pricing to RestaurantCard

--- FoodSwipe/components/RestaurantCard.js
+++ FoodSwipe/components/RestaurantCard.js
@@ -9,6 +9,7 @@ import {
 	View,
 	Image
 } from 'react-native';
+const DOLLAR_IMAGE = require('FoodSwipe/images/dollar_symbol.png');
 //designs the inner box for the swipe CardSection
 //react elements was heavly used for this section to render the picture and the stars
 
@@ -18,28 +19,37 @@ class RestaurantCard extends React.Component {
 		super(props)
 	}
 
+	renderStarsRating(rating){
+		console.log(rating);
+		return (
+			<Rating ratingBackgroundColor='red' imageSize={40}
+				style={{ paddingVertical: 10 }} fractions={1}
+				type="star" startingValue={rating}
+			/>
+		);
+	}
+
+	renderPriceRating(price){
+		// Add 1 since values range 0-4
+		price += 1;
+		return (
+			<Rating ratingBackgroundColor='red' imageSize={40}
+				style={{ paddingVertical: 10 }} fractions={1}
+				type="custom" startingValue={price}
+				ratingImage={DOLLAR_IMAGE}
+			/>
+		);
+	}
+
 	render(){
 		const restaurant = this.props
 		return(
 		<View style={[styles.card, {backgroundColor: '#FFC30F'}]}>
-
-<Tile
-   imageSrc={{uri:restaurant.photo}}
-	 height ={300}
-		width={300}
-/>
-
-			 <Text>{restaurant.name}</Text>
-			 <Rating
-
-				 type="star"
-				 fractions={1}
-				 startingValue={restaurant.rating}
-				 ratingBackgroundColor='red'
-				 imageSize={40}
-
-				 style={{ paddingVertical: 10 }}
-			 />
+			<Tile height={300} width={300}
+				 imageSrc={{uri:restaurant.photo}} />
+			<Text>{restaurant.name}</Text>
+			 { this.renderStarsRating(restaurant.rating)}
+			 { this.renderPriceRating(restaurant.price)}
 		 </View>)
 	}
 }
